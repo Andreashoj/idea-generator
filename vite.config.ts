f390decc8731a9a5c6204347c138cf1f003f7bec
Andreas Høj <andrewhoj@gmail.com>
2022-10-24 16:18:26 +0200

    updated icons fra pwa build

--- vite.config.ts
+++ vite.config.ts
@@ -39,6 +39,12 @@ export default defineConfig({
             src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "pwa-icon-transparent-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
             purpose: "any maskable",
           },
         ],
