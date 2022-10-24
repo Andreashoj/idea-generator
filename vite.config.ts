2e8706f48ed7e127f7a4cffd3d72481013516d83
Andreas Høj <andrewhoj@gmail.com>
2022-10-24 16:05:40 +0200

    manifest

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,33 @@ export default defineConfig({
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
       },
+      injectRegister: "script",
+      manifest: {
+        name: "Idea Generator",
+        short_name: "Idea Gen",
+        theme_color: "#FFDA44",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#FFDA44",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
     }),
   ],
   resolve: {
