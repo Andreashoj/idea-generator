e6a7e53e285ea5fb3376da6ed205ae4ea0646236
Andreas Høj <andrewhoj@gmail.com>
2022-10-24 16:23:00 +0200

    icons for build

--- vite.config.ts
+++ vite.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
             type: "image/png",
           },
           {
-            src: "pwa-512x512.png",
+            src: "pwa-icon-transparent-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any",
